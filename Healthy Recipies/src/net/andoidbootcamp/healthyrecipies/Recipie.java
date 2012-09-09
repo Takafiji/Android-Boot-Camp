9018b4c99c85cdd8c8515af9b0418228da98aec2
Takafiji <rossb237@gmail.com>
2012-09-08 20:05:48 -0600

    first commit

---
+++ Healthy Recipies/src/net/andoidbootcamp/healthyrecipies/Recipie.java
@@ -0,0 +1,14 @@
+package net.andoidbootcamp.healthyrecipies;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Recipie extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.recipies);
+		
+	}
+}
